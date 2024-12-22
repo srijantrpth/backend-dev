e7bee6437fa90b2767c96957916dea620d79bdc5
Srijan Tripathi <srijantrpth@gmail.com>
2024-12-22 17:05:34 +0530

    Basic Routing Setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,10 @@
-const asyncHandler = (requestHandler) => async(err, req, res, next) => {
+export const asyncHandler = (requestHandler) => async(err, req, res, next) => {
     try {
         await fn()(req,res,next)
     } catch (error) {
         res.status(err.code || 500).json({success: false, message: err.message})
+        
     }
+
 }
 
