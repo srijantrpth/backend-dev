f9cf94191a657823ed0d9706d9115b344088405a
Srijan Tripathi <srijantrpth@gmail.com>
2024-12-23 18:58:09 +0530

    Add refresh token endpoint and export in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -164,4 +164,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
